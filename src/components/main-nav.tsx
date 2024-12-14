5f6df05bd5689c1884730ee92d5e85b41850dd93
Databayt <osmanabdout@hotmail.com>
2024-12-14 11:56:34 +0300

    x

---
+++ src/components/main-nav.tsx
@@ -0,0 +1,113 @@
+"use client"
+
+import Link from "next/link"
+import { usePathname } from "next/navigation"
+
+import { siteConfig } from "@/config/site"
+import { cn } from "@/lib/utils"
+import { Icons } from "@/components/icons"
+
+export function MainNav() {
+  const pathname = usePathname()
+
+  return (
+    <div className="mr-4 hidden md:flex">
+      <Link href="/" className="mr-4 flex items-center space-x-2 lg:mr-6">
+        <Icons.logo className="h-6 w-6" />
+        <span className="hidden font-bold lg:inline-block">
+          {siteConfig.name}
+        </span>
+      </Link>
+      <nav className="flex items-center gap-4 text-sm lg:gap-6">
+        <Link
+          href="/docs"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname === "/docs" ? "text-foreground" : "text-foreground/60"
+          )}
+        >
+          Docs
+        </Link>
+        <Link
+          href="/docs/components"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname?.startsWith("/docs/components") &&
+              !pathname?.startsWith("/docs/component/chart")
+              ? "text-foreground"
+              : "text-foreground/60"
+          )}
+        >
+          Atom
+        </Link>
+        <Link
+          href="/blocks"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname?.startsWith("/blocks")
+              ? "text-foreground"
+              : "text-foreground/60"
+          )}
+        >
+          Template
+        </Link>
+        <Link
+          href="/charts"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname?.startsWith("/docs/component/chart") ||
+              pathname?.startsWith("/charts")
+              ? "text-foreground"
+              : "text-foreground/60"
+          )}
+        >
+          Block
+        </Link>
+        <Link
+          href="/themes"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname?.startsWith("/themes")
+              ? "text-foreground"
+              : "text-foreground/60"
+          )}
+        >
+          Micro
+        </Link>
+        <Link
+          href="/examples"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname?.startsWith("/examples")
+              ? "text-foreground"
+              : "text-foreground/60"
+          )}
+        >
+          Icon
+        </Link>
+        <Link
+          href="/colors"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname?.startsWith("/colors")
+              ? "text-foreground"
+              : "text-foreground/60"
+          )}
+        >
+          font
+        </Link>
+        <Link
+          href="/colors"
+          className={cn(
+            "transition-colors hover:text-foreground/80",
+            pathname?.startsWith("/colors")
+              ? "text-foreground"
+              : "text-foreground/60"
+          )}
+        >
+          figma
+        </Link>
+      </nav>
+    </div>
+  )
+}
---
+++ src/components/providers.tsx
@@ -0,0 +1,16 @@
+"use client"
+
+import * as React from "react"
+import { Provider as JotaiProvider } from "jotai"
+import { ThemeProvider as NextThemesProvider, ThemeProviderProps } from "next-themes"
+import { TooltipProvider } from "@/components/ui/tooltip"
+
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return (
+    <JotaiProvider>
+      <NextThemesProvider {...props}>
+        <TooltipProvider delayDuration={0}>{children}</TooltipProvider>
+      </NextThemesProvider>
+    </JotaiProvider>
+  )
+}
