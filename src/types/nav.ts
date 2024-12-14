edd21dd6e5dde3faba881e9061148c7395a98e25
Databayt <osmanabdout@hotmail.com>
2024-12-14 13:48:27 +0300

    x

--- src/types/nav.ts
+++ src/types/nav.ts
@@ -1,24 +1,18 @@
-// src/types/nav.ts
-import { Icons } from "@/components/icons";
+import { Icons } from "@/components/icons"
 
 export interface NavItem {
-  title: string;
-  href?: string;
-  disabled?: boolean;
-  external?: boolean;
-  icon?: keyof typeof Icons;
-  label?: string;
+  title: string
+  href?: string
+  disabled?: boolean
+  external?: boolean
+  icon?: keyof typeof Icons
+  label?: string
 }
 
 export interface NavItemWithChildren extends NavItem {
-  items: NavItemWithChildren[];
+  items: NavItemWithChildren[]
 }
 
-export type MainNavItem = NavItem;
+export interface MainNavItem extends NavItem {}
 
-// If SidebarNavItem is ALSO empty after extending, use a type alias here too
-// Check if you are adding any properties to SidebarNavItem in other files.
-// If not, change this to:
-// export type SidebarNavItem = NavItemWithChildren;
-
-export interface SidebarNavItem extends NavItemWithChildren {} // Only if it has unique props
+export interface SidebarNavItem extends NavItemWithChildren {}
