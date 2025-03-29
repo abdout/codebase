15dcaf2e8e0e9d447914039558ef9a01dc62621d
abdout <osmanabdout@hotmail.com>
2025-03-29 05:42:08 +0300

    x

--- apps/micro/next.config.ts
+++ apps/micro/next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  basePath: '/micro',
+  output: 'standalone',
 };
 
 export default nextConfig;
